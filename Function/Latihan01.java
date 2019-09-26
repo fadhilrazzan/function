759854ded483aad33c0809d7bc8e780c5d25c9e5
fadhilrazzan <54299052+fadhilrazzan@users.noreply.github.com>
2019-09-26 21:11:40 +0700

    Add files via upload

---
+++ Function/Latihan01.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latihan01;
+
+import java.util.Scanner;
+
+
+
+/**
+ *
+ * @author Lenovo
+ */
+public class Latihan01{
+   public static void hitung(int a, int b){
+       System.out.println("Hasil :"+ (a + b));
+       {
+    Scanner Nemu = new Scanner(System.in);
+    
+    System.out.print("Masukkan Bilangan I : ");
+    int bil1 = Nemu.nextInt();
+    
+    System.out.print("Masukkan Bilangan II : ");
+    int bil2 = Nemu.nextInt();
+    
+    hitung = (bil1, bil2);
+       }
+   }
+}
